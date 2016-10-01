5c1fd25ff9d1bd5ae94cfc8818b2959b25758540
xolodutra <pavelsahnov@yandex.ru>
2016-10-01 15:50:08 +0300

    Доработали бота, добавили ответы по словарю

--- bot.py
+++ bot.py
@@ -1,21 +1,24 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from answers import get_answer, answers
 
-def start(bot, update):
-    print("Вызван /start")
-    bot.sendMessage(update.message.chat_id, text='Ну здорова, коль не шутишь!')
+def start111(bot, update):
+     print("Вызван /start")
+     bot.sendMessage(update.message.chat_id, text='Ну здорова, коль не шутишь!')
 
 def talk_to_my(bot, update):
-    print("Пришло сообщение: %s" %update.message.text)
-    bot.sendMessage(update.message.chat_id, text=update.message.text)
+    print("Пришло сообщение: %s" % update.message.text)
+    text = get_answer(update.message.text, answers)
+    bot.sendMessage(update.message.chat_id, text=text)
 
 
 def run_bot():
     updater = Updater("269779371:AAGAKo2IhxvWeDpR2wUKSDzo_VO43BzMpyE")
 
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("start", start111))
     dp.add_handler(MessageHandler([Filters.text], talk_to_my))
-    
+
+
     updater.start_polling()
     updater.idle()
 
