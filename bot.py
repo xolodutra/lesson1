4c075d70cfe445b316d3c564a88a8b0cbfb8775f
xolodutra <pavelsahnov@yandex.ru>
2016-10-05 23:18:52 +0300

    добавил файл с функцией калькулятором

--- bot.py
+++ bot.py
@@ -1,21 +1,43 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from answers import get_answer, answers
 
-def start111(bot, update):
+def start(bot, update):
      print("Вызван /start")
      bot.sendMessage(update.message.chat_id, text='Ну здорова, коль не шутишь!')
 
+
+def count_word(bot, update):
+    msg = update.message.text
+    msg = len(msg.split(' ')) - 1
+    text_count = 'Введено {} слов'.format(msg)
+    bot.sendMessage(update.message.chat_id, text=text_count)
+
+
+
 def talk_to_my(bot, update):
     print("Пришло сообщение: %s" % update.message.text)
-    text = get_answer(update.message.text, answers)
-    bot.sendMessage(update.message.chat_id, text=text)
+    if '=' in text:
+        bot.sendMessage(update.message.chat_id)
+    else:
+        text = get_answer(update.message.text, answers)
+        bot.sendMessage(update.message.chat_id, text=text)
 
+# def talk_to_me(bot, update):
+#    print("Пришло сообщение: " + update.message.text)
+#    user_text = update.message.text
+#    if "=" in user_text:
+#        bot.sendMessage(update.message.chat_id, text = solver(user_text))
+#    else:    
+#        bot.sendMessage(update.message.chat_id, text = answer.answer(user_text))
 
 def run_bot():
     updater = Updater("269779371:AAGAKo2IhxvWeDpR2wUKSDzo_VO43BzMpyE")
 
+
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start111))
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("calculat", calculat))
+    dp.add_handler(CommandHandler("count", count_word))
     dp.add_handler(MessageHandler([Filters.text], talk_to_my))
 
 
---
+++ calculator.py
@@ -0,0 +1,26 @@
+def calculate():
+    task = input('Введите задачу:__')
+    if '=' in task:
+        repl_task = task.replace('=', '')
+        if '+' in repl_task:
+            numbers = repl_task.split("+")
+            x, y = numbers
+            print(str(int(x)+int(y)))
+        elif '-' in repl_task:
+            numbers = repl_task.split("-")
+            i, j = numbers
+            print(str(int(i)-int(j)))
+        elif '*' in repl_task:
+            numbers = repl_task.split("*")
+            a, b = numbers
+            print(str(int(a)*int(b)))
+        elif '/' in repl_task:
+            numbers = repl_task.split("/")
+            o, v = numbers
+            print(str(int(o)/int(v)))
+        else:
+            print('пока')
+    else:
+        print('вы забыли знак равно')
+calculate()
+
