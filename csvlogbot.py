ce0507e8d5b998db4dfaf3b1d5ba687febccf29a
xolodutra <pavelsahnov@yandex.ru>
2016-10-30 22:30:22 +0300

    делаю логирование в csv

--- csvlogbot.py
+++ csvlogbot.py
@@ -5,7 +5,10 @@ with open('girl_names.csv', 'r', encoding='utf-8')  as f:
     fields = ['system_object_id_','global_id_','NumberOfPersons_','Year_','Month_','Name_','ID']
     # fields = [date, time, username, content]
     reader = csv.DictReader(f, fields, delimiter =';' )
+    year_total = 0
+
     for row in reader:
-        print(row)
+        year_total += float(row['Year_'])
+    print(year_total)
 
 
