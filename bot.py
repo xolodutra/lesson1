20dd82688584a2670a8b874de1d982383ea57d94
xolodutra <pavelsahnov@yandex.ru>
2016-10-30 03:40:55 +0300

    заработало логирование обращений к боту

--- bot.py
+++ bot.py
@@ -5,13 +5,8 @@ from foolmoon import fool_moon_metr
 from log_bot import output_reader
 import ephem
 
-# Вот функция, которая передаёт данные в функцию log_bot
-def log_writer(bot, update):
-    output_log =  ['first_name', 'text' ]
-    text = output_reader(update.message, output_log)
-    print("что-то начало получаться?")
-    bot.sendMessage(update.message.chat_id, text=text)
-
+def log_writer(from_user, to_user, username):
+    return output_reader(from_user, to_user, username)
 
 # Ввод команды /start в телеграм вызывает:
 def start(bot, update):
@@ -19,12 +14,7 @@ def start(bot, update):
      to_user = "Ну здорова, коль не шутишь!"
      print(from_user)
      bot.sendMessage(update.message.chat_id, text=to_user)
-     log_writer(from_user, to_user, update.message)
-
-# def start(bot, update):
-#      print("Вызван /start")
-#      print(update.message)
-#      bot.sendMessage(update.message.chat_id, text='Ну здорова, коль не шутишь!')
+     log_writer(from_user, to_user, update.message.chat.username)
 
 # Ввод команды /calculat вызывает :
 def solver(bot, update):
@@ -38,28 +28,31 @@ def count_word(bot, update):
     msg = len(msg.split(' ')) - 1
     text_count = 'Введено {} слов'.format(msg)
     bot.sendMessage(update.message.chat_id, text=text_count)
+    try:
+        log_writer(msg, text_count, update.message.chat.username)
+    except Exception as e:
+        print(e)
 
 # Ввод слов из списка вызывает: 
 def talk_to_my(bot, update):
     print("Пришло сообщение: %s" % update.message.text)
-
     if update.message.text in answers:
         text = get_answer(update.message.text, answers)
+        log_writer(update.message.text, text ,update.message.chat.username)
     else:
         text = fool_moon_metr(update.message.text)
+        log_writer(update.message.text, text ,update.message.chat.username)
     bot.sendMessage(update.message.chat_id, text=text)
 
 # Функция управления общением с ботом
 def run_bot():
     updater = Updater("269779371:AAGAKo2IhxvWeDpR2wUKSDzo_VO43BzMpyE")
 
-
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("calc", solver))
     dp.add_handler(CommandHandler("count", count_word))
     dp.add_handler(MessageHandler([Filters.text], talk_to_my))
-
 
     updater.start_polling()
     updater.idle()
--- log_bot.py
+++ log_bot.py
@@ -1,32 +1,19 @@
 from datetime import datetime
 
+def output_reader(from_user, to_user, username ):
 
-def output_reader(output_log, chat_out):
-    chat_out = {}
-    # output_log =  ['first_name', 'text' ]
-    output_log = []
     # начинаем с добавления текущей даты
     dt_now = datetime.now()
     dt_now = dt_now.strftime('%d.%m.%Y  %H:%m')
     out_list = [dt_now]
-    i = 0
-    # проходим по списку и находим каждый из его элементов в словаре, 
-    # котоый выдаёт телеграм в update.message
-    while i < len(output_log):
-        och = output_log[i]
-        log_info = chat_out.get(och)
-        out_list.append(log_info)
-        i = i + 1
-        if i >= len(output_log):
-            break
-    out_str = '\n{}'. format('\t'.join(out_list) )
 
+    out_list.append(str(username))
+    out_list.append(str(from_user))
+    out_list.append(str(to_user))
+
+    out_str = '\n{}'. format('\t'.join(out_list))
     with open('log_bot.txt', 'a', encoding='utf-8')  as logfile:
         logfile.write(out_str)
 
-
 if __name__ == "__main__":
-    output_reader("прпрпрп", "прпрпрп")
-
-
-
+    output_reader("from_user", "to_user", "chat_out")
